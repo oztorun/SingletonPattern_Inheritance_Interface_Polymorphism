330b6dacbacd023ff864b47cac4e96844b646217
Özgür <ozgurtorun1996@gmail.com>
2018-09-16 20:58:59 +0300

    Singleton Pattern and OOP's Princible

---
+++ Inheritance/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Inheritance
+{
+    class Vehicle
+    {
+        string name;
+        int enginepower;
+        int torque;
+        int wheel;
+
+        public Vehicle(string _name,int _torque, int _enginepower, int _wheel)
+        {
+            name = _name;
+            enginepower = _enginepower;
+            torque = _torque;
+            wheel = -wheel;
+        }
+
+        public int truePower()
+        {
+            return (enginepower * torque) / 100;
+        }
+
+
+    }
+
+   
+
+    class Motorcycle : Vehicle
+    {
+
+        public Motorcycle(string _name, int _torque,int  _enginepower,int _wheel)
+            : base(_name,_torque,_enginepower,_wheel)
+        { }
+
+        public int balance()
+        {
+            return truePower() / 2;
+        }
+    
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle vehicle1 = new Vehicle("Mercedes", 350, 220, 4);
+            Vehicle vehicle2 = new Vehicle("BMW", 300, 250, 4);
+
+            Console.WriteLine("*****Vehicle Spefications******");
+
+            Console.WriteLine("First Car True Power : " + vehicle1.truePower());
+            Console.WriteLine("Second Car True Power : " + vehicle2.truePower());
+
+            Motorcycle motorcycle1 = new Motorcycle("Yamaha", 420, 120, 2);
+            Motorcycle motorcycle2 = new Motorcycle("Honda", 370, 130, 2);
+
+            Console.WriteLine("******Motorcycle Spefications*****");
+
+            Console.WriteLine("First MotorCycle True Power:" + motorcycle1.truePower());
+            Console.WriteLine("Second MotorCycle True Power:" + motorcycle2.truePower());
+
+            Console.WriteLine("*****0About Balance For MotorCycle******");
+            Console.WriteLine("First MotorCycle Balance =" + motorcycle1.balance());
+            Console.WriteLine("Second MotorCycle Balance =" + motorcycle2.balance());
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace SingletonPattern
+{
+    public class LoadBalancer
+    {
+        private static LoadBalancer _instance;
+        private static object m_SyncObject = new object();
+        private List<Server> m_ServerList = new List<Server>();
+
+        private LoadBalancer()
+        {
+            Server server = new Server("10.75.5.1", 10001);
+            Server emailServer = new Server("10.75.5.1", 10001);
+            Server emailWebServer = new Server("10.75.5.1", 10001);
+            Server smsServer = new Server("10.75.5.1", 10001);
+
+            m_ServerList.Add(server);
+            m_ServerList.Add(emailServer);
+            m_ServerList.Add(emailWebServer);
+            m_ServerList.Add(smsServer);
+        }
+        
+        public List<Server> GetServers()
+        {
+            return m_ServerList;
+        }
+
+        public static LoadBalancer Instance()
+        {
+            if (_instance != null)
+                return _instance;
+
+            lock (m_SyncObject)
+            {
+                if (_instance == null)
+                {
+                    _instance = new LoadBalancer();
+                }
+            }
+            return _instance;
+        }
+    }
+
+    public class Server
+    {
+        private string m_IP;
+        private int m_Port;
+
+        public Server(string ip, int port)
+        {
+            m_IP = ip;
+            m_Port = port;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 5;
+             Thread thread2 = new Thread(new ThreadStart(executeNewThread));
+             thread2.Start();
+            
+            Console.WriteLine("main thread working");
+            LoadBalancer loadBalancer1 = LoadBalancer.Instance();
+            //LoadBalancer loadBalancer2 = LoadBalancer.Instance();
+
+            //if (loadBalancer1 == loadBalancer2)
+            //{
+            //    Console.Write("Nesneler aynı durumda");
+            //}
+
+            Console.ReadKey();
+
+        }
+
+        private static void executeNewThread()
+        {
+            LoadBalancer loadBalancer2 = LoadBalancer.Instance();
+        }
+    }
+}
